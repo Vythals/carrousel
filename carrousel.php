4539b8c73fbf3a1dabed1cda7bae4d0cd0ef7ec4
Vythals <Vythals@users.noreply.github.com>
2023-05-01 11:47:36 -0500

    ajout des boutons suivant précédent

--- carrousel.php
+++ carrousel.php
@@ -6,10 +6,9 @@
  * Version: 1.0.0
  */
 
-
 function mon_enqueue_css_js(){
     $version_css = filemtime(plugin_dir_path( __FILE__ ) . "style.css");
-    $version_js = filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js");    
+    $version_js = filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js");
 
     wp_enqueue_style( 'em_plugin_carrousel_css',
                      plugin_dir_url(__FILE__) . "style.css",
@@ -18,10 +17,9 @@ function mon_enqueue_css_js(){
 
     wp_enqueue_script(  'em_plugin_carrousel_js',
                     plugin_dir_url(__FILE__) . "js/carrousel.js",
-                    array(),
+                    array('jquery'),
                     $version_js,
                     true);
-
 }
 
 add_action('wp_enqueue_scripts', 'mon_enqueue_css_js');
@@ -29,12 +27,22 @@ add_action('wp_enqueue_scripts', 'mon_enqueue_css_js');
 
 function creation_carrousel()
 {
-    return "<button class='carrousel__ouvrir'>Ouvrir le carrousel</button>
-    <div class='carrousel'>
-    <button class='carrousel__x'>X</button>
-    <figure class='carrousel__figure'></figure>
-    <form class='carrousel__form'></form>
-    </div> <!-- fin du carrousel -->
+    $images = get_post_gallery_images();
+    $html_images = '';
+    foreach ($images as $index => $url) {
+        $html_images .= "<img src='{$url}' class='carrousel__img' alt='image-{$index}'>";
+    }
+    $html = "<button class='carrousel__ouvrir'>Ouvrir le carrousel</button>
+        <div class='carrousel'>
+            <button class='carrousel__x'>X</button>
+            <div class='carrousel__container'>
+                <figure class='carrousel__figure'>{$html_images}</figure>
+                <form class='carrousel__form'></form>
+            </div>
+            <button class='carrousel__precedent'>Précédent</button>
+            <button class='carrousel__suivant'>Suivant</button>
+        </div> <!-- fin du carrousel -->
     ";
+    return $html;
 }
 add_shortcode('carrousel', 'creation_carrousel');
--- js/carrousel.js
+++ js/carrousel.js
@@ -1,50 +1,187 @@
-const images = document.querySelectorAll(".galerie__img");
-const carrousel = document.querySelector(".carrousel");
-const carrouselImg = document.querySelector(".carrousel__img");
-const carrouselClose = document.querySelector(".carrousel__x");
-let currentIndex = 0;
+(function(){
+   console.log('Début du carrousel');
+   let carrousel__ouvrir = document.querySelector('.carrousel__ouvrir');
+   let carrousel = document.querySelector('.carrousel');
+   let carrousel__x = document.querySelector('.carrousel__x');
+   let carrousel__figure = document.querySelector('.carrousel__figure');
+   let carrousel__form = document.querySelector('.carrousel__form');
+   let galerie = document.querySelector('.galerie');
+   let galerie__img = galerie.querySelectorAll('img');
+ 
+   let position = 0;
+   let index = 0;
+   let ancienIndex = -1;
+   let imagesAdded = false; // Nouvelle variable pour indiquer si les images ont déjà été ajoutées
+ 
+   carrousel__ouvrir.addEventListener('mousedown', function(){
+     carrousel.classList.add('carrousel--activer');
+     if (!imagesAdded) { // Ajouter les images seulement s'ils n'ont pas déjà été ajoutés
+       ajouter_les_images_de_galerie();
+       imagesAdded = true;
+     }
+   });
+ 
+   carrousel__x.addEventListener('mousedown', function(){
+     carrousel.classList.remove('carrousel--activer');
+   });
+ 
+   galerie__img.forEach(function(elem) { // Ajouter l'écouteur d'événement à chaque image de la galerie
+     elem.addEventListener('click', function() {
+       carrousel.classList.add('carrousel--activer');
+       if (!imagesAdded) { // Ajouter les images seulement s'ils n'ont pas déjà été ajoutés
+         ajouter_les_images_de_galerie();
+         imagesAdded = true;
+       }
+       index = elem.dataset.index; // Utiliser l'index de l'image cliquée
+       affiche_image_carrousel();
+     });
+   });
+ 
+   function ajouter_les_images_de_galerie() {
+     for (const elem of galerie__img){
+       ajouter_une_image_dans_courrousel(elem);
+       ajouter_un_radio_bouton_dans_carrousel();
+     }
+   }
+ 
+   function ajouter_une_image_dans_courrousel(elem) {
+     let img = document.createElement('img');
+     img.classList.add('carrousel__img');
+     img.src = elem.src;
+     carrousel__figure.appendChild(img);
+   }
+ 
+   function ajouter_un_radio_bouton_dans_carrousel() {
+     let rad = document.createElement('input');
+     rad.setAttribute('type', 'radio');
+     rad.setAttribute('name', 'carrousel__rad');
+     rad.classList.add('carrousel__rad');
+     rad.dataset.index = position;
+     rad.addEventListener('mousedown', function(){
+       index =  this.dataset.index;
+       affiche_image_carrousel();
+     });
+     position = position + 1;
+     carrousel__form.append(rad);
+   }
+ 
+   function affiche_image_carrousel() {
+     if (ancienIndex != -1){
+       carrousel__figure.children[ancienIndex].style.opacity = "0";
+     }
+     carrousel__figure.children[index].style.opacity = "1";
+     ancienIndex = index;
+   }
+ })();
+ function afficherImage(index) {
+   const images = document.querySelectorAll(".galerie__image img");
+   const imageCarrousel = document.querySelector(".carrousel__figure");
+   const formulaire = document.querySelector(".carrousel__form");
+   const nbImages = images.length;
+   let indice = index;
+   
+   if (indice >= nbImages) {
+       indice = 0;
+   } else if (indice < 0) {
+       indice = nbImages - 1;
+   }
 
-// Ajout des images au carrousel
-function addImages() {
-  images.forEach((image, index) => {
-    const img = document.createElement("img");
-    img.src = image.src;
-    img.alt = image.alt;
-    img.dataset.index = index;
-    img.addEventListener("click", () => {
-      currentIndex = index;
-      showCarrousel();
-    });
-    carrousel.appendChild(img);
-  });
+   const image = images[indice];
+   const imageUrl = image.getAttribute("src");
+   const imageAlt = image.getAttribute("alt");
+   
+   imageCarrousel.style.backgroundImage = `url(${imageUrl})`;
+   imageCarrousel.style.backgroundSize = "contain";
+   imageCarrousel.style.backgroundPosition = "center";
+   imageCarrousel.style.backgroundRepeat = "no-repeat";
+   
+   formulaire.innerHTML = `<h2>${imageAlt}</h2>`;
+   
+   const boutonPrecedent = document.querySelector(".carrousel__precedent");
+   const boutonSuivant = document.querySelector(".carrousel__suivant");
+   
+   if (nbImages > 1) {
+       boutonPrecedent.style.display = "block";
+       boutonSuivant.style.display = "block";
+       boutonPrecedent.addEventListener("click", () => {
+           afficherImage(indice - 1);
+       });
+       boutonSuivant.addEventListener("click", () => {
+           afficherImage(indice + 1);
+       });
+   } else {
+       boutonPrecedent.style.display = "none";
+       boutonSuivant.style.display = "none";
+   }
+   
+   document.querySelector(".carrousel").classList.add("carrousel--activer");
 }
-
-// Affichage du carrousel
-function showCarrousel() {
-  carrouselImg.src = images[currentIndex].src;
-  carrouselImg.alt = images[currentIndex].alt;
-  carrousel.classList.add("carrousel--activer");
-}
-
-// Fermeture du carrousel
-function closeCarrousel() {
-  carrousel.classList.remove("carrousel--activer");
-}
-
-// Navigation dans le carrousel
-function navigateCarrousel(event) {
-  if (event.key === "ArrowLeft") {
-    currentIndex = (currentIndex - 1 + images.length) % images.length;
-    carrouselImg.src = images[currentIndex].src;
-    carrouselImg.alt = images[currentIndex].alt;
-  } else if (event.key === "ArrowRight") {
-    currentIndex = (currentIndex + 1) % images.length;
-    carrouselImg.src = images[currentIndex].src;
-    carrouselImg.alt = images[currentIndex].alt;
-  }
-}
-
-// Ajout des écouteurs d'événements
-addImages();
-carrouselClose.addEventListener("click", closeCarrousel);
-document.addEventListener("keydown", navigateCarrousel);
+jQuery(document).ready(function($) {
+   // Variables globales
+   var index = 0; // Index de l'image actuellement affichée
+ 
+   // Cacher le bouton "Ouvrir le carrousel" au chargement de la page
+   $('.carrousel__ouvrir').hide();
+ 
+   // Gérer le clic sur une image pour afficher le carrousel
+   $('.galerie__img').click(function() {
+     // Récupérer l'index de l'image cliquée
+     index = $(this).index();
+     // Afficher le carrousel avec l'image cliquée
+     afficherCarrousel(index);
+   });
+ 
+   // Gérer le clic sur le bouton "Suivant"
+   $('.carrousel__suivant').click(function() {
+     // Incrémenter l'index de l'image
+     index++;
+     // Si on dépasse la dernière image, revenir à la première
+     if (index >= $('.galerie__img').length) {
+       index = 0;
+     }
+     // Afficher l'image suivante dans le carrousel
+     afficherImage(index);
+   });
+ 
+   // Gérer le clic sur le bouton "Précédent"
+   $('.carrousel__precedent').click(function() {
+     // Décrémenter l'index de l'image
+     index--;
+     // Si on dépasse la première image, revenir à la dernière
+     if (index < 0) {
+       index = $('.galerie__img').length - 1;
+     }
+     // Afficher l'image précédente dans le carrousel
+     afficherImage(index);
+   });
+ 
+   // Fonction pour afficher le carrousel avec une image spécifique
+   function afficherCarrousel(index) {
+     // Afficher le carrousel
+     $('.carrousel').addClass('carrousel--activer');
+     // Afficher l'image spécifique dans le carrousel
+     afficherImage(index);
+   }
+ 
+   // Fonction pour afficher une image spécifique dans le carrousel
+   function afficherImage(index) {
+     // Récupérer l'image correspondant à l'index
+     var $img = $('.galerie__img').eq(index);
+     // Récupérer l'URL de l'image
+     var url = $img.attr('src');
+     // Afficher l'image dans le carrousel
+     $('.carrousel__img').attr('src', url).animate({opacity: 1}, 500);
+     // Mettre à jour le texte alternatif de l'image
+     $('.carrousel__img').attr('alt', $img.attr('alt'));
+   }
+ 
+   // Gérer le clic sur le bouton "Fermer"
+   $('.carrousel__x').click(function() {
+     // Cacher le carrousel
+     $('.carrousel').removeClass('carrousel--activer');
+     // Réinitialiser l'opacité de l'image
+     $('.carrousel__img').css('opacity', 0);
+   });
+ });
+ 
+ 
